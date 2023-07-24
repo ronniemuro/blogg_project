b3790601f6007bb9cddf945d81eda2e72501d6ec
Ronnie Lopez-Muro <ronniethan@outlook.com>
2023-07-23 21:29:51 -0600

    first commit work on 07/23

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          RonnieBlogg
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://variety.com/wp-content/uploads/2023/07/F0SWVnOaYAELt_H.jpg" alt="" />
+        </div>
+        <div className="text">
+          <h2> Red, White, & Royal Blue MOVIE!!</h2>
+          <p className="info">
+            <a className="author">Ronnie Lopez-Muro</a>
+            <time> 2023-07-23 20:54</time>
+          </p>
+          <p className="summary">
+            the movie is coming sooner than we think, here are a few reminders on one of the most iconic scenes from the
+            book that we hope it will be adapted
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://variety.com/wp-content/uploads/2023/07/F0SWVnOaYAELt_H.jpg" alt="" />
+        </div>
+        <div className="text">
+          <h2> Red, White, & Royal Blue MOVIE!!</h2>
+          <p className="info">
+            <a className="author">Ronnie Lopez-Muro</a>
+            <time> 2023-07-23 20:54</time>
+          </p>
+          <p className="summary">
+            the movie is coming sooner than we think, here are a few reminders on one of the most iconic scenes from the
+            book that we hope it will be adapted
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://variety.com/wp-content/uploads/2023/07/F0SWVnOaYAELt_H.jpg" alt="" />
+        </div>
+        <div className="text">
+          <h2> Red, White, & Royal Blue MOVIE!!</h2>
+          <p className="info">
+            <a className="author">Ronnie Lopez-Muro</a>
+            <time> 2023-07-23 20:54</time>
+          </p>
+          <p className="summary">
+            the movie is coming sooner than we think, here are a few reminders on one of the most iconic scenes from the
+            book that we hope it will be adapted
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
